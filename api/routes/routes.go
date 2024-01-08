0457b5763805c4d895aadf6f25a875f1001e0438
Luan A. <barkon32@tuta.io>
2024-01-08 12:39:32 -0300

    initiated chi router and create ong route

--- api/routes/routes.go
+++ api/routes/routes.go
@@ -1,2 +1,15 @@
 package routes
 
+import (
+	"pet-dex-backend/v2/api/controllers"
+
+	"github.com/go-chi/chi"
+)
+
+func Routes() *chi.Mux {
+	router := chi.NewRouter()
+
+	router.Post("/api/ongs", controllers.HandleCreateOng)
+
+	return router
+}
