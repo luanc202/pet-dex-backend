82a00898203e3b2be429a77522fe51811c1daf83
Luan A. <barkon32@tuta.io>
2024-01-09 18:47:49 -0300

    Revert "added code to run server"
    This reverts commit 3eff4e9026093b9913e1800e30b59b9c98f712bc.

--- cmd/main.go
+++ cmd/main.go
@@ -1,24 +1,13 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	"net/http"
-
-	"pet-dex-backend/v2/api/routes"
+	"pet-dex-backend/v2/infra/db"
+	"pet-dex-backend/v2/usecase"
 )
 
 func main() {
-	dbc, err := sql.Open("mysql", "root:root@tcp")
-	if err != nil {
-		panic(err)
-	}
-	defer dbc.Close()
-	// or := db.NewOngRepository()
-	port := "3000"
-	srv := &http.Server{
-		Addr:    fmt.Sprintf(":%s", port),
-		Handler: routes.Routes(),
-	}
-	srv.ListenAndServe()
+	pr := db.NewPetRepository()
+	adoptUseCase := usecase.NewAdoptUseCase(pr)
+
+	adoptUseCase.Do()
 }
